dac47cb0c2b4530c750586f6992facaff94a1ec0
Gabriel Ribeiro Siqueira <gabrielrsiqueira2023@gmail.com>
2024-03-30 01:54:05 -0300

    feat: finish the LinkedList in go. Insert prev and display

--- Go/LinkedList/list.go
+++ Go/LinkedList/list.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "fmt"
+
 type node struct {
 	prev *node
 	data int
@@ -25,4 +27,30 @@ func (l *LinkedList) InsertNext(data int) {
 
 	current.next = newNode
 	newNode.prev = current
+}
+
+func (l *LinkedList) InsertPrev(data int) {
+	newNode := &node{prev: nil, data: data, next: nil}
+
+	if l.head == nil {
+		l.head = newNode
+	}
+
+	newNode.next = l.head
+	l.head.prev = newNode
+	l.head = newNode
+}
+
+func (l *LinkedList) Display() {
+	if l.head == nil {
+		fmt.Print("Empty list!")
+		return
+	}
+
+	current := l.head
+
+	for current != nil {
+		fmt.Printf("Valor: %v", current.data)
+		current = current.next
+	}
 }
