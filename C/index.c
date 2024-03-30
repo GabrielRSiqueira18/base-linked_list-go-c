5624db6d4e554013b3301634afd7625770832e4b
Gabriel Ribeiro Siqueira <gabrielrsiqueira2023@gmail.com>
2024-03-30 01:32:44 -0300

    feat: create insertNext function, to add node in the end of the list.

--- C/index.c
+++ C/index.c
@@ -7,4 +7,44 @@ typedef struct Node {
   struct Node *next;
 } node;
 
-int main() {}
+typedef struct LinkedList {
+  node *head;
+} linkedList;
+
+node* createNode(int data) {
+  node *newNode = (node*) malloc(sizeof(node*));
+
+  if (newNode = NULL) {
+    printf("No memory!");
+    exit(1);
+  }
+
+  newNode->prev = NULL;
+  newNode->data = data; 
+  newNode->prev = NULL;
+
+  return newNode;
+}
+
+void insertNext(int data, linkedList **list) {
+  node *newNode = createNode(data);
+
+  linkedList *current = *list;
+
+  if (current->head = NULL) {
+    current->head = newNode;
+    return;
+  } 
+
+  node *temp = current->head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+
+int main() {
+  
+}
